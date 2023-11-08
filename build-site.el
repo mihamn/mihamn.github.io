32aa4c416f20255bd17abdca21e2126f7ff516f3
Mark Mihajlovic <markmihajlovic@marks-air-2.home>
2023-11-08 17:59:30 +0000

    Change home.org to index.org

--- build-site.el
+++ build-site.el
@@ -30,7 +30,7 @@
              :recursive t
              :base-directory "./content"
              :publishing-function 'org-html-publish-to-html
-             :publishing-directory "./public"
+             :publishing-directory "./docs"
              :with-author nil           ;; Don't include author name
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc t                ;; Include a table of contents
